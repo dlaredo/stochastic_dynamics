f051b9a81654bc42a2c00f1e98742c3cbf155dbd
dlaredo <davidlaredo1@gmail.com>
2019-02-27 16:59:13 -0800

    Fixing error in the residual function

--- code/loss_functions.py
+++ code/loss_functions.py
@@ -137,8 +137,8 @@ def squared_residual_function_wrapper2(k, c, D, deltas, num_feval):
         R4 = R4_integral(y_pred_delta2_plus, y_pred_delta2_minus, delta_x, delta_y)
         R5 = R5_integral(y_pred_original, y_pred_delta2_plus, y_pred_delta2_minus, delta_x, delta_y)
         
-        #R_total = R1 + c - c*R2 - k*R3 + D*tf.subtract(R4, R5)
-        R_total = R2
+        R_total = R1 + c*delta_x*delta_y - c*R2 - k*R3 + D*tf.subtract(R4, R5)
+        #R_total = R2
         
         
         R = tf.reduce_sum(tf.pow(R_total, 2)) / (2 * tf.cast(batch_size, tf.float32))
