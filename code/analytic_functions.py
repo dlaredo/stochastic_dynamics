465c62d621600e86c7af3c04741399066b48c160
dlaredo <davidlaredo1@gmail.com>
2019-05-20 16:29:29 -0700

    New test notebook for DE's
    Minor changes to some DE definition.

--- code/analytic_functions.py
+++ code/analytic_functions.py
@@ -9,13 +9,20 @@ def real_p(x1, x2, sigma_x, sigma_y):
 
     return np.reshape(p, [p.shape[0], 1]), np.reshape(phi, [phi.shape[0], 1])
 
+def ode1(x):
+
+    y = np.exp(-x) + 1
+
+    return np.reshape(y, [y.shape[0], 1])
+
+
 def real_y(x):
 
-#    y = np.exp(-x)+1
+    y = np.exp(-x)+1
     
     # examples
     #1
-    y = np.exp(-(np.power(x, 2)/2)) / (1 + x + np.power(x, 3)) + np.power(x, 2)
+    #y = np.exp(-(np.power(x, 2)/2)) / (1 + x + np.power(x, 3)) + np.power(x, 2)
 #     #2
 #     y = np.exp(-x/5) * np.sin(x)
 
